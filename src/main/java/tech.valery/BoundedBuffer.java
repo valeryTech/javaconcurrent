d97336df40bf26cb87ade9416b895309bde47978
valeryTech <mail@valery.tech>
2018-05-30 09:26:10 +0700

    Add BoundedBuffer
    BoundedBuffer is to serve as a base to various custom synchronizers.

--- src/main/java/tech.valery/BoundedBuffer.java
+++ src/main/java/tech.valery/BoundedBuffer.java
@@ -1,10 +1,19 @@
 package tech.valery;
 
+import net.jcip.annotations.GuardedBy;
+
 public class BoundedBuffer<V> {
 
+    @GuardedBy("this")
     private int count;
+
+    @GuardedBy("this")
     private V[] data;
+
+    @GuardedBy("this")
     private int tail;
+
+    @GuardedBy("this")
     private int head;
 
     public BoundedBuffer(int capacity) {
@@ -16,16 +25,21 @@ public class BoundedBuffer<V> {
     }
 
     public synchronized void doPut(V v) {
+        try {
+            this.wait(10, 100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         data[tail] = v;
-        if(++tail == data.length)
+        if (++tail == data.length)
             tail = 0;
         ++count;
     }
 
-    public synchronized V doTake(){
+    public synchronized V doTake() {
         V v = data[head];
 
-        if(++head == data.length)
+        if (++head == data.length)
             head = 0;
         --count;
 
--- src/test/java/tech.valery/BoundedBufferTest.java
+++ src/test/java/tech.valery/BoundedBufferTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BoundedBufferTest {
+
     @Test
     void ShouldCheckForEmptiness_WhenBufferIsJustCreated(){
         int capacity = 1;
