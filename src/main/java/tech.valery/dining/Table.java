c497a6d6a3d08a0969a8c01dafbc11021b30a73e
valeryTech <mail@valery.tech>
2018-06-01 14:04:57 +0700

    Add factory methods to Table constructor
    Now Table uses factory methods for creating new instances of Philosophers and Sticks.
    
    Injecting is in test methods.

--- src/main/java/tech.valery/dining/Table.java
+++ src/main/java/tech.valery/dining/Table.java
@@ -1,13 +1,15 @@
 package tech.valery.dining;
 
 import tech.valery.dining.chopsticks.Chopstick;
-import tech.valery.dining.chopsticks.LockChopstick;
+import tech.valery.dining.philosophers.ChandyPhilosopher;
 import tech.valery.dining.philosophers.Philosopher;
 
 import java.util.Arrays;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 public class Table {
 
@@ -20,14 +22,18 @@ public class Table {
 
     private final Chopstick[] sticks;
 
-    public Table(int participantsNumber) {
+    public Table(int participantsNumber, Supplier<Chopstick> stickSupplier,
+                 BiFunction<Integer, Table, Philosopher> philosopherFactory) {
         this.participantsNumber = participantsNumber;
 
         chopstickIsInFreeState = new boolean[participantsNumber];
         Arrays.fill(chopstickIsInFreeState, Boolean.TRUE);
 
-        sticks = new LockChopstick[participantsNumber];
-        Arrays.setAll(sticks, i -> new LockChopstick());
+        sticks = new Chopstick[participantsNumber];
+        Arrays.setAll(sticks, i -> stickSupplier.get());
+
+        Philosopher[] philosophers = new ChandyPhilosopher[participantsNumber];
+        Arrays.setAll(philosophers, i -> philosopherFactory.apply(i, this));
     }
 
     public Chopstick getRightChopstick(int seat) {
@@ -65,5 +71,9 @@ public class Table {
 
     private boolean bothSticksIsFree(Philosopher philosopher) {
         return chopstickIsInFreeState[philosopher.getSeat()] && chopstickIsInFreeState[philosopher.getSeat() + 1];
+    }
+
+    public void startSimulation() {
+
     }
 }
--- src/test/java/tech.valery/dining/TableTest.java
+++ src/test/java/tech.valery/dining/TableTest.java
@@ -1,73 +1,47 @@
 package tech.valery.dining;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tech.valery.Common;
 import tech.valery.dining.chopsticks.Chopstick;
+import tech.valery.dining.chopsticks.LockChopstick;
 import tech.valery.dining.chopsticks.MisraStatefulStick;
 import tech.valery.dining.philosophers.ChandyPhilosopher;
 import tech.valery.dining.philosophers.DependentPhilosopher;
 import tech.valery.dining.philosophers.Philosopher;
 
-import java.util.Arrays;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
 public class TableTest {
 
-    @Test
-    void PhilosopherShouldWaitUntilAllChopsticksIsAvailable_WhenItWantsToEat() throws InterruptedException {
-        int problemSize = 5;
+    private Supplier<Chopstick> stickSupplier;
+    private BiFunction<Integer, Table, Philosopher> philosopherFactory;
+    private Table table;
+    private int problemSize;
 
-        Table table = new Table(problemSize);
-
-        Philosopher[] philosophers = new DependentPhilosopher[problemSize];
-        Arrays.setAll(philosophers, i -> new DependentPhilosopher(i, table));
-
-        Philosopher lockingPhilosopher = philosophers[0];
-        Philosopher asquiringPhilosoper = philosophers[1];
-
-        lockingPhilosopher.prepareToEat();
-
-        CompletableFuture.runAsync(()->{
-            try {
-                asquiringPhilosoper.prepareToEat();
-                Common.sleep(10);
-                asquiringPhilosoper.prepareToThink();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        Common.sleep(10);
-        lockingPhilosopher.prepareToThink();
+    @BeforeEach
+    void initMonitoringSystem(){
+        table = new Table(problemSize, stickSupplier, philosopherFactory);
     }
 
     @Test
     void ShouldRunWithoutDeadlocks_WhenUsingArbitrator() {
-        int problemSize = 5;
+        problemSize = 5;
 
-        Table table = new Table(problemSize);
+        stickSupplier = LockChopstick::new;
+        philosopherFactory =
+                (Integer seat, Table table) -> new DependentPhilosopher(seat, table);
 
-        Philosopher[] philosophers = new DependentPhilosopher[problemSize];
-        Arrays.setAll(philosophers, i -> new DependentPhilosopher(i, table));
-
-        Arrays.stream(philosophers).forEach(CompletableFuture::runAsync);
-
-        Common.sleep(2000);
+        table.startSimulation();
     }
 
     @Test
     void ShouldRundWithoutDeadlocks_WhenUsingChandyMisraSolution() {
-        int problemSize = 5;
-        Supplier<Chopstick> stickSupplier = MisraStatefulStick::new;
-        //todo add supplier
-        Table table = new Table(problemSize);
+        problemSize = 5;
 
-        Philosopher[] philosophers = new ChandyPhilosopher[problemSize];
-        Arrays.setAll(philosophers, i -> new ChandyPhilosopher(i, table));
+        stickSupplier = MisraStatefulStick::new;
+        philosopherFactory = (Integer seat, Table table) -> new ChandyPhilosopher(seat, table);
 
-        Arrays.stream(philosophers).forEach(CompletableFuture::runAsync);
-
-        Common.sleep(2000);
+        table.startSimulation();
     }
 }
