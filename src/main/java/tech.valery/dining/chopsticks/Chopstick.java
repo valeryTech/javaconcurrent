94a5512a605b2c8caeb0ced9c83aeb807c4a0ce8
valeryTech <mail@valery.tech>
2018-06-01 12:24:25 +0700

    Clean Chopstick interface
    There is no need to hold the link to current philosopher.

--- src/main/java/tech.valery/dining/chopsticks/Chopstick.java
+++ src/main/java/tech.valery/dining/chopsticks/Chopstick.java
@@ -1,17 +1,7 @@
 package tech.valery.dining.chopsticks;
 
-import tech.valery.dining.philosophers.OrderedPhilosopher;
-
 public interface Chopstick {
     void take() throws InterruptedException;
 
     void put() throws InterruptedException;
-
-    void setHolder(OrderedPhilosopher philosopher);
-
-    OrderedPhilosopher getHolder();
-
-    Boolean isGotten();
-
-    int getId();
 }
