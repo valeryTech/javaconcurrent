83ca36d0d4f42a664610eaf3c92cdd51c8047e3d
valeryTech <mail@valery.tech>
2018-06-04 19:25:33 +0700

    Add stick for Chandy/Misra solution

--- src/main/java/tech.valery/dining/chopsticks/MisraStatefulStick.java
+++ src/main/java/tech.valery/dining/chopsticks/MisraStatefulStick.java
@@ -2,11 +2,42 @@ package tech.valery.dining.chopsticks;
 
 import net.jcip.annotations.GuardedBy;
 
-public class MisraStatefulStick extends LockChopstick {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MisraStatefulStick implements Chopstick{
 
     @GuardedBy("this")
     private boolean isClean;
 
+    @GuardedBy("lock")
+    private boolean free = true;
+
+    public final Lock lock = new ReentrantLock();
+
+    public final Condition hasFreed = lock.newCondition();
+
     public MisraStatefulStick() {
+        setClean(true);
+    }
+
+    public boolean isClean() {
+        return isClean;
+    }
+
+    public void setClean(boolean clean) {
+        isClean = clean;
+    }
+
+    @Override
+    public void pickUp() throws InterruptedException {
+        lock.lock();
+    }
+
+    @Override
+    public void putDown() {
+        lock.unlock();
+        isClean = false;
     }
 }
--- src/main/java/tech.valery/dining/philosophers/ChandyPhilosopher.java
+++ src/main/java/tech.valery/dining/philosophers/ChandyPhilosopher.java
@@ -8,10 +8,18 @@ public class ChandyPhilosopher extends Philosopher{
     @Override
     public void prepareToEat() {
 
+        //neighbours
+
+        // obtain the absent forks from contending neighbours
     }
 
     @Override
     public void prepareToThink() throws InterruptedException {
-
+        // check all the requests give up dirty,
+        // if dirty -> clean & send
+        // if clean -> keep
     }
+
+    // override eat -> forks shoud be dirty after 1 use
+
 }
