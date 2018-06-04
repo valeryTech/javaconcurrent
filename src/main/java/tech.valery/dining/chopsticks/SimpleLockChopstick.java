b82978f9cf1777192870f83e8dfcb3b1577c02d1
valeryTech <mail@valery.tech>
2018-06-04 18:59:51 +0700

    Add SimpleChopstick
    The holder now is tracked by an implicit lock behaviour.

---
+++ src/main/java/tech.valery/dining/chopsticks/SimpleLockChopstick.java
@@ -0,0 +1,16 @@
+package tech.valery.dining.chopsticks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SimpleLockChopstick {
+    private final Lock lock = new ReentrantLock();
+
+    public void pickUp(){
+        lock.lock();
+    }
+
+    public void putDown(){
+        lock.unlock();
+    }
+}
